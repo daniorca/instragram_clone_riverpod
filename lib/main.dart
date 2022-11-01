c7933881d60a9e9bce21597cac03664e9194767c
Daniel Ortizc <daniel.ortizc99@gmail.com>
2022-10-31 18:54:46 -0600

    authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
-//import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:instagram_clone_course/state/auth/providers/auth_state_provider.dart';
+import 'package:instagram_clone_course/state/auth/providers/is_logged_in_provider.dart';
+import 'firebase_options.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'dart:developer' as devtools show log;
 
-void main() {
-  runApp(MyApp()
-      // const ProviderScope(
-      //   child: MyApp(),
-      // ),
-      );
+extension Log on Object {
+  void log() => devtools.log(toString());
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -15,21 +29,92 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Riverpod',
-      darkTheme: ThemeData.dark(),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blueGrey,
+        indicatorColor: Colors.blueGrey,
+      ),
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+      ),
       themeMode: ThemeMode.dark,
-      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+      home: Consumer(builder: (context, ref, child) {
+        final isLoading = ref.watch(authStateProvider).isLoading;
+        final isLoggedIn = ref.watch(isLoggedInProvider);
+        if (isLoading) {
+          return const LoadingView();
+        }
+        if (isLoggedIn) {
+          return const MainView();
+        } else {
+          return const LoginView();
+        }
+      }),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+// when you are already logged in
+class MainView extends StatelessWidget {
+  const MainView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Instagram Clone'),
+        title: const Text('Main View'),
+      ),
+      body: Consumer(builder: (context, ref, child) {
+        return TextButton(
+          onPressed: () async {
+            await ref.read(authStateProvider.notifier).logOut();
+          },
+          child: Text('Logout'),
+        );
+      }),
+    );
+  }
+}
+
+class LoginView extends StatelessWidget {
+  const LoginView({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login View'),
+      ),
+      body: Consumer(builder: (context, ref, child) {
+        return Column(
+          children: <Widget>[
+            TextButton(
+              onPressed: ref.read(authStateProvider.notifier).loginWithGoogle,
+              child: const Text('Sign In with Goggle'),
+            ),
+            TextButton(
+              onPressed: ref.read(authStateProvider.notifier).loginWithFacebook,
+              child: const Text('Sign In with Facebook'),
+            )
+          ],
+        );
+      }),
+    );
+  }
+}
+
+class LoadingView extends StatelessWidget {
+  const LoadingView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
       ),
     );
   }
